1b85106d5f7b0466ef6d86869ea5bf270363fa57
Kevin Tapia <k.tapiabaeza.it@gmail.com>
2020-08-16 12:32:52 -0400

    docs agregado

---
+++ docs/precache-manifest.953383a7046b3293fe93e856fde9daf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7317e6af01bea6c6b9b1a81237b46866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc455061e7b6c4597dc",
+    "url": "/static/css/main.0c260b5f.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc455061e7b6c4597dc",
+    "url": "/static/js/main.f0f32c48.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
